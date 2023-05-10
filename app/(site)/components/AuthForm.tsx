c339f77146f3de6f781f7150f57f8a048a934d53
Harrieson <harriemburu@gmail.com>
2023-05-10 23:50:10 +0300

    Login/Register Modals

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Input from "@/app/components/inputs/Input";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+
+type Variant = "LOGIN" | "REGISTER";
+const AuthForm =() => {
+    const [variant, setVariant] = useState<Variant>("LOGIN");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if(variant === "LOGIN") {
+            setVariant("REGISTER")
+        }else{
+            setVariant("LOGIN")
+        }
+    }, [variant])
+
+
+    const {register, handleSubmit, formState: { errors}} = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> =(data) => {
+        setIsLoading(true);
+
+        if(variant === "REGISTER") {
+
+        }
+
+        if(variant === "LOGIN") {
+
+        }
+    }
+
+    const socialAction = (action: string) => {
+        setIsLoading(true)
+    }
+    return (
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+                <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                    <Input label="Email" register={register} id="email"/>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default AuthForm;
